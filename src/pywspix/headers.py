771c74ba28e6becac26dbefbeb952bfe6bba8a11
Marcelo Schcneider <schneider.fei@gmail.com>
2025-07-25 14:30:39 -0300

    Adiciona documentação para a classe Headers

--- src/pywspix/headers.py
+++ src/pywspix/headers.py
@@ -1,11 +1,28 @@
 class Headers:
-    def __init__(self, user, password):
+    """
+    Gera os cabeçalhos HTTP de autenticação para chamadas à API Pix.
+
+    Essa classe encapsula o usuário e a senha, retornando um dicionário
+    de cabeçalhos customizados exigidos pelo serviço.
+    """
+
+    def __init__(self, user: str, password: str):
+        """
+        Inicializa o gerador de headers com credenciais de autenticação.
+
+        Args:
+            user (str): Nome de usuário (X-Username).
+            password (str): Senha de autenticação (X-Password).
+        """
         self.__user = user
         self.__password = password
 
-    def generate(self):
-        header_content = {
-            "X-Username": self.__user,
-            "X-Password": self.__password
-        }
+    def generate(self) -> dict:
+        """
+        Gera o dicionário de cabeçalhos HTTP com as credenciais.
+
+        Returns:
+            dict: Cabeçalhos com campos `X-Username` e `X-Password`.
+        """
+        header_content = {"X-Username": self.__user, "X-Password": self.__password}
         return header_content
